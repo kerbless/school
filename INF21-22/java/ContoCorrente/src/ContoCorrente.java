bf26273957c6c0e61df61edd9d4429af6dd6c38d
kerbless <bluburro7@gmail.com>
2021-10-30 10:18:18 +0200

    Finished ContoCorrente

--- INF21-22/java/ContoCorrente/src/ContoCorrente.java
+++ INF21-22/java/ContoCorrente/src/ContoCorrente.java
@@ -1,3 +1,23 @@
 public class ContoCorrente {
-    
+    private String intestatario;
+    private int codice;
+    private float saldo;
+
+    ContoCorrente(String intestatario, int codice) {
+        this.intestatario = intestatario;
+        this.codice = codice;
+        saldo = 0;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void prelievo(float ammontare) {
+        saldo -= ammontare;
+    }
+
+    public void versamento(float ammontare) {
+        saldo += ammontare;
+    }
 }
